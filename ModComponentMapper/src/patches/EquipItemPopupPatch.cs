a890eb563fa294fb6284d09b70a27c94261c38be
WulfMarius <WulfMarius@users.noreply.github.com>
2018-12-23 21:09:26 +0100

    fixed ammo display for modded tool items

---
+++ ModComponentMapper/src/patches/EquipItemPopupPatch.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using ModComponentAPI;
+using UnityEngine;
+
+namespace ModComponentMapper
+{
+    [HarmonyPatch(typeof(EquipItemPopup), "AllowedToHideAmmoPopup")]
+    internal class EquipItemPopup_AllowedToHideAmmoPopup
+    {
+        internal static void Postfix(ref bool __result)
+        {
+            if (__result)
+            {
+                return;
+            }
+
+            __result = ModUtils.GetModComponent(GameManager.GetPlayerManagerComponent().m_ItemInHands) != null;
+        }
+    }
+}
