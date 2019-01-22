025896b281ddcbabf992885c6be57626eb2cc567
WulfMarius <WulfMarius@users.noreply.github.com>
2019-01-22 21:23:03 +0100

    use BreakOnUse to add a popup message

--- ModComponentMapper/src/patches/FireManagerPatches.cs
+++ ModComponentMapper/src/patches/FireManagerPatches.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Harmony;
+﻿using Harmony;
 using ModComponentAPI;
 
 namespace ModComponentMapper
 {
     [HarmonyPatch(typeof(FireManager), "PlayerStartFire")]
-    class FireManager_PlayerStartFire
+    internal class FireManager_PlayerStartFire
     {
         internal static void Postfix(FireStarterItem starter, bool __result)
         {
@@ -27,7 +22,7 @@ namespace ModComponentMapper
             GearItem gearItem = starter.GetComponent<GearItem>();
             if (gearItem != null)
             {
-                gearItem.Degrade(gearItem.m_MaxHP);
+                gearItem.BreakOnUse();
             }
         }
     }
