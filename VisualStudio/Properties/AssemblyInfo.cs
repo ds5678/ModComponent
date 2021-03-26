1d4030c95877eba88a26c06ea7948ff20896acb0
ds5678 <jrpritts7@gmail.com>
2021-03-26 16:32:46 -0400

    updated assembly info

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.3.0")]
-[assembly: AssemblyFileVersion("4.3.0")]
-[assembly: MelonInfo(typeof(ModComponentMapper.Implementation), "ModComponent", "4.3.0", "Wulf Marius, ds5678")]
+[assembly: AssemblyVersion("4.3.1")]
+[assembly: AssemblyFileVersion("4.3.1")]
+[assembly: MelonInfo(typeof(ModComponentMapper.Implementation), "ModComponent", "4.3.1", "Wulf Marius, ds5678")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
