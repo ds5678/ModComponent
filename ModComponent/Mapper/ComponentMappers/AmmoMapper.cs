82bfc645d0871934039d28be6507145da26e5f47
DeadmanWasTaken <trent.sandford2005@gmail.com>
2021-12-24 20:53:49 +1030

    Added AmmoComponent
    Broken, even though there are no errors. Adding this to your .json mod doesn't turn the item into a "bullet"

---
+++ ModComponent/Mapper/ComponentMappers/AmmoMapper.cs
@@ -0,0 +1,16 @@
+﻿using ModComponent.API.Components;
+
+namespace ModComponent.Mapper.ComponentMappers
+{
+	internal static class AmmoMapper
+	{
+		internal static void Configure(ModBaseComponent modComponent)
+		{
+			ModAmmoComponent modAmmoCompnent = modComponent.TryCast<ModAmmoComponent>();
+			if (modAmmoCompnent == null) return;
+
+			AmmoItem ammoItem = ModComponent.Utils.ComponentUtils.GetOrCreateComponent<AmmoItem>(modComponent);
+            ammoItem.m_AmmoForGunType = ModComponent.Utils.EnumUtils.TranslateEnumValue<GunType, ModAmmoComponent.GunType>(modAmmoCompnent.AmmoForGunType);
+        }
+	}
+}
