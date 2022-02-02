ee802425dcefde7b963b52df046b525ccab3a4f4
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-02 18:18:27 -0500

    bump version

--- ModComponent/BuildInfo.cs
+++ ModComponent/BuildInfo.cs
@@ -4,9 +4,9 @@
 	{
 		public const string Name = "ModComponent"; // Name of the Mod.  (MUST BE SET)
 		public const string Description = "A utility mod for custom item creation."; // Description for the Mod.  (Set as null if none)
-		public const string Author = "ds5678, WulfMarius"; // Author of the Mod.  (MUST BE SET)
+		public const string Author = "ds5678"; // Author of the Mod.  (MUST BE SET)
 		public const string Company = null; // Company that made the Mod.  (Set as null if none)
-		public const string Version = "5.1.1"; // Version of the Mod.  (MUST BE SET)
+		public const string Version = "5.1.2"; // Version of the Mod.  (MUST BE SET)
 		public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
 	}
 }
