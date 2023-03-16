9d42f683aca5ae8fb708f16ccb033a366a77d7ee
STBlade <dommrogers@gmail.com>
2023-03-16 20:49:00 +0000

    version bump 6.0.0

--- ModComponent/BuildInfo.cs
+++ ModComponent/BuildInfo.cs
@@ -6,6 +6,6 @@ public static class BuildInfo
 	public const string Description = "A utility mod for custom item creation."; // Description for the Mod.  (Set as null if none)
 	public const string Author = "ds5678"; // Author of the Mod.  (MUST BE SET)
 	public const string Company = null; // Company that made the Mod.  (Set as null if none)
-	public const string Version = "5.1.4"; // Version of the Mod.  (MUST BE SET)
+	public const string Version = "6.0.0"; // Version of the Mod.  (MUST BE SET)
 	public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
 }
